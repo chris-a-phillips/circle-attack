6363593174aa6e4e13a26cf43d97e8435111ee1c
atlfinesse <finesse.cap@gmail.com>
2021-01-29 08:59:44 -0800

    Project set up

---
+++ script.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+}
