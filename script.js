f3ebe46e6604f392277f5beac4850f7de6c61e04
atlfinesse <finesse.cap@gmail.com>
2021-01-29 09:47:57 -0800

    Animated projectiles statically

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Player {
 	constructor(x, y, radius, color) {
@@ -14,9 +14,9 @@ class Player {
 
 	draw() {
 		context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        context.fillStyle = this.color;
-        context.fill();
+		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		context.fillStyle = this.color;
+		context.fill();
 	}
 }
 
@@ -35,16 +35,34 @@ class Projectile {
 		context.fillStyle = this.color;
 		context.fill();
 	}
+
+	update() {
+		this.draw();
+		this.x = this.x + this.velocity.x;
+		this.y = this.y + this.velocity.y;
+	}
 }
 
-const x = canvas.width / 2
-const y = canvas.height / 2
+const x = canvas.width / 2;
+const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-
 player.draw()
 
+const projectiles = [];
+
+function animate() {
+    requestAnimationFrame(animate);
+    projectiles.forEach((projectile) => {
+        projectile.update();
+    });
+}
+
 window.addEventListener('click', (event) => {
-    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
-    projectile.draw()
-})
+    projectiles.push(new Projectile(canvas.width /2, canvas.height / 2, 5, 'red', {
+        x: 1,
+        y: 1
+    }))
+});
+
+animate()
