db94d7d71172e12a815b8eac76ac9ff8d2f4ad1b
atlfinesse <finesse.cap@gmail.com>
2021-01-29 11:07:52 -0800

    Removed off screen projectiles

--- script.js
+++ script.js
@@ -44,21 +44,9 @@ class Projectile {
 }
 
 class Enemy extends Projectile {
-    constructor(
-        x,
-        y,
-        radius,
-        color,
-        velocity
-    ) {
-        super(
-            x,
-            y,
-            radius,
-            color,
-            velocity
-        )
-    }
+	constructor(x, y, radius, color, velocity) {
+		super(x, y, radius, color, velocity);
+	}
 }
 
 const x = canvas.width / 2;
@@ -66,85 +54,98 @@ const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
 const projectiles = [];
-const enemies = []
+const enemies = [];
 
 function spawnEnemies() {
-    setInterval(() => {
-        const radius = Math.random() * (30 - 4) + 4;
-        let x
-        let y
+	setInterval(() => {
+		const radius = Math.random() * (30 - 4) + 4;
+		let x;
+		let y;
 
-        if (Math.random() < 0.5) {
-            x = (Math.random() < .5 ? 0 - radius : canvas.width + radius);
-            y = Math.random() * canvas.height;
-        } else {
-            x = Math.random() * canvas.width;
-            y = (Math.random() < .5 ? 0 - radius : canvas.height + radius);
-        }
+		if (Math.random() < 0.5) {
+			x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+			y = Math.random() * canvas.height;
+		} else {
+			x = Math.random() * canvas.width;
+			y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+		}
 
-        const color = 'green'
-        const angle = Math.atan2(
-            canvas.height / 2 - y,
-            canvas.width / 2 - x
-        );
-        const velocity = {
-            x: Math.cos(angle),
-            y: Math.sin(angle),
-        };
+		const color = 'green';
+		const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+		const velocity = {
+			x: Math.cos(angle),
+			y: Math.sin(angle),
+		};
 
-        enemies.push(new Enemy(x, y, radius, color, velocity))
-    }, 1000)
+		enemies.push(new Enemy(x, y, radius, color, velocity));
+	}, 1000);
 }
 
 // GAME OVER FRAME
-let animationId
+let animationId;
 // GAME RUN FUNCTION
 function animate() {
-    animationId = requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+	animationId = requestAnimationFrame(animate);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	player.draw();
 	projectiles.forEach((projectile) => {
 		projectile.update();
-    });
 
-    // COLLISION DETECTION FOR PLAYER
+		// IF PROJECTILE LEAVES SCREEN STOP COMPUTATION FOR IT
+		if (
+			projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height
+		) {
+			setTimeout(() => {
+				projectiles.splice(projectiles.indexOf(projectile), 1);
+			}, 0);
+		}
+	});
+
+	// COLLISION DETECTION FOR PLAYER
 	enemies.forEach((enemy) => {
-        enemy.update();
-        
-        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+		enemy.update();
 
-        if (distance - enemy.radius - player.radius < 1) {
-            cancelAnimationFrame(animationId)
-        }
+		const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
 
-        // COLLISION DETECTION FOR PROJECTILES
-        projectiles.forEach(projectile => {
-            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+		if (distance - enemy.radius - player.radius < 1) {
+			cancelAnimationFrame(animationId);
+		}
 
-            if (distance - enemy.radius - projectile.radius < 1) {
-                // GETS RID OF FLASH WHEN ENEMIES ARE SPLICED
-                setTimeout(() => {
-                    enemies.splice(enemies.indexOf(enemy),1)
-                    projectiles.splice(projectiles.indexOf(projectile),1)
-                }, 0);
-            }
-        })
-    });
+		// COLLISION DETECTION FOR PROJECTILES
+		projectiles.forEach((projectile) => {
+			const distance = Math.hypot(
+				projectile.x - enemy.x,
+				projectile.y - enemy.y
+			);
+
+			if (distance - enemy.radius - projectile.radius < 1) {
+				// GETS RID OF FLASH WHEN ENEMIES ARE SPLICED
+				setTimeout(() => {
+					enemies.splice(enemies.indexOf(enemy), 1);
+					projectiles.splice(projectiles.indexOf(projectile), 1);
+				}, 0);
+			}
+		});
+	});
 }
 
 window.addEventListener('click', (event) => {
+    console.log(projectiles)
 	const angle = Math.atan2(
 		event.clientY - canvas.height / 2,
 		event.clientX - canvas.width / 2
-    );
-    const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
-    }
+	);
+	const velocity = {
+		x: Math.cos(angle),
+		y: Math.sin(angle),
+	};
 	projectiles.push(
 		new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
 	);
 });
 
 animate();
-spawnEnemies()
+spawnEnemies();
