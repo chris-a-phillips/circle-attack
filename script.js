3771ead740e1f144b8b22951c87f415c4cf2af71
atlfinesse <finesse.cap@gmail.com>
2021-01-29 11:16:03 -0800

    Randomized color and sped up projectile speed

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ class Enemy extends Projectile {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 10, 'white');
 const projectiles = [];
 const enemies = [];
 
@@ -68,9 +68,9 @@ function spawnEnemies() {
 		} else {
 			x = Math.random() * canvas.width;
 			y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
-		}
+        }
 
-		const color = 'green';
+		const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 		const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 		const velocity = {
 			x: Math.cos(angle),
@@ -85,8 +85,9 @@ function spawnEnemies() {
 let animationId;
 // GAME RUN FUNCTION
 function animate() {
-	animationId = requestAnimationFrame(animate);
-	context.clearRect(0, 0, canvas.width, canvas.height);
+    animationId = requestAnimationFrame(animate);
+    context.fillStyle = 'rgba(0, 0, 0, 0.1)'
+	context.fillRect(0, 0, canvas.width, canvas.height);
 	player.draw();
 	projectiles.forEach((projectile) => {
 		projectile.update();
@@ -137,13 +138,14 @@ window.addEventListener('click', (event) => {
 	const angle = Math.atan2(
 		event.clientY - canvas.height / 2,
 		event.clientX - canvas.width / 2
-	);
+    );
+    // PROJECTILE VELOCITY
 	const velocity = {
-		x: Math.cos(angle),
-		y: Math.sin(angle),
+		x: Math.cos(angle) * 5,
+		y: Math.sin(angle) * 5,
 	};
 	projectiles.push(
-		new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+		new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
 	);
 });
 
